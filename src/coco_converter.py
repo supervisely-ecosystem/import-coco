f0236cd1ea3d0b2344c7354b1a55583df784f85f
Ars Vladimirov <grokhael@gmail.com>
2023-07-19 17:03:57 +0600

    add checktype

--- src/coco_converter.py
+++ src/coco_converter.py
@@ -4,41 +4,69 @@ import shutil
 import numpy as np
 import pycocotools.mask as mask_util
 import supervisely as sly
+from pycocotools.coco import COCO
 from PIL import Image
 from supervisely.io.fs import file_exists, mkdir
+from typing import List
 
 import globals as g
 
 def add_tail(body:str, tail:str):
-    return f"{body} ({tail})"
+    if ' ' in body:
+        return f"{body} {tail}"
+    return f"{body}_{tail}"
+    
 
-def create_sly_meta_from_coco_categories(coco_categories):
+def get_ann_types(coco: COCO) -> List[str]:
+    ann_types = []
+
+    sly.logger.info('Getting info about annotation types..')
+
+    annotation_ids = coco.getAnnIds()
+    if any('bbox' in coco.anns[ann_id] for ann_id in annotation_ids):
+        ann_types.append('bbox')
+    if any('segmentation' in coco.anns[ann_id] for ann_id in annotation_ids):
+        ann_types.append('segmentation')
+
+    return ann_types
+
+def create_sly_meta_from_coco_categories(coco_categories, ann_types=None):
     colors = []
     for category in coco_categories:
         if category["name"] in [obj_class.name for obj_class in g.META.obj_classes]:
             continue
         new_color = sly.color.generate_rgb(colors)
         colors.append(new_color)
-        
-        obj_class_polygon = sly.ObjClass(add_tail(category['name'], "polygon"), sly.Polygon, new_color)
-        obj_class_rectangle = sly.ObjClass(add_tail(category['name'], "rectangle"), sly.Rectangle, new_color)
-        g.META = g.META.add_obj_classes([obj_class_polygon, obj_class_rectangle])
+
+        obj_classes = []
+
+        if ann_types is not None:
+            if 'segmentation' in ann_types:
+                obj_classes.append(
+                    sly.ObjClass(category['name'], sly.Polygon, new_color)
+                )
+            if 'bbox' in ann_types:
+                obj_classes.append(
+                    sly.ObjClass(add_tail(category['name'], "bbox"), sly.Rectangle, new_color)
+                )
+
+        g.META = g.META.add_obj_classes(obj_classes)
     return g.META
 
 
 
 
-def get_sly_meta_from_coco(coco_categories, dataset_name):
+def get_sly_meta_from_coco(coco_categories, dataset_name, ann_types=None):
     path_to_meta = os.path.join(g.SLY_BASE_DIR, "meta.json")
     if not os.path.exists(path_to_meta):
-        g.META = dump_meta(coco_categories, path_to_meta)
+        g.META = dump_meta(coco_categories, path_to_meta, ann_types)
     elif dataset_name not in ["train2014", "val2014", "train2017", "val2017"]:
-        g.META = dump_meta(coco_categories, path_to_meta)
+        g.META = dump_meta(coco_categories, path_to_meta, ann_types)
     return g.META
 
 
-def dump_meta(coco_categories, path_to_meta):
-    g.META = create_sly_meta_from_coco_categories(coco_categories)
+def dump_meta(coco_categories, path_to_meta, ann_types=None):
+    g.META = create_sly_meta_from_coco_categories(coco_categories, ann_types)
     meta_json = g.META.to_json()
     sly.json.dump_json_file(meta_json, path_to_meta)
     return g.META
@@ -82,31 +110,36 @@ def create_sly_ann_from_coco_annotation(meta, coco_categories, coco_ann, image_s
     labels = []
     for object in coco_ann:
         name_cat_id_map = coco_category_to_class_name(coco_categories)
-        obj_class_name_polygon = add_tail( name_cat_id_map[object["category_id"]], "polygon")
-        obj_class_polygon = meta.get_obj_class(obj_class_name_polygon)
-        if type(object["segmentation"]) is dict:
-            polygons = convert_rle_mask_to_polygon(object)
-            for polygon in polygons:
-                figure = polygon
+
+        segm = object.get("segmentation")
+        if segm is not None:
+            obj_class_name_polygon = name_cat_id_map[object["category_id"]]
+            obj_class_polygon = meta.get_obj_class(obj_class_name_polygon)
+
+            if type(segm) is dict:
+                polygons = convert_rle_mask_to_polygon(object)
+                for polygon in polygons:
+                    figure = polygon
+                    label = sly.Label(figure, obj_class_polygon)
+                    labels.append(label)
+            elif type(segm) is list and object["segmentation"]:
+                figure = convert_polygon_vertices(object)
                 label = sly.Label(figure, obj_class_polygon)
                 labels.append(label)
-        elif type(object["segmentation"]) is list and object["segmentation"]:
-            figure = convert_polygon_vertices(object)
-            label = sly.Label(figure, obj_class_polygon)
-            labels.append(label)
 
-        bbox = object["bbox"]
-        xmin = bbox[0]
-        ymin = bbox[1]
-        xmax = xmin + bbox[2]
-        ymax = ymin + bbox[3]
+        bbox = object.get("bbox")
+        if bbox is not None:
+            xmin = bbox[0]
+            ymin = bbox[1]
+            xmax = xmin + bbox[2]
+            ymax = ymin + bbox[3]
 
-        obj_class_name_rectangle = add_tail( name_cat_id_map[object["category_id"]], "rectangle")
-        obj_class_rectangle = meta.get_obj_class(obj_class_name_rectangle)
-        rectangle = sly.Label(
-            sly.Rectangle(top=ymin, left=xmin, bottom=ymax, right=xmax), obj_class_rectangle
-        )
-        labels.append(rectangle)
+            obj_class_name_rectangle = add_tail( name_cat_id_map[object["category_id"]], "bbox")
+            obj_class_rectangle = meta.get_obj_class(obj_class_name_rectangle)
+            rectangle = sly.Label(
+                sly.Rectangle(top=ymin, left=xmin, bottom=ymax, right=xmax), obj_class_rectangle
+            )
+            labels.append(rectangle)
     return sly.Annotation(image_size, labels)
 
 
